df4886676b26f4a40a065dfb6da174fe19aaa176
Nitish Kumar <iamnitishsah12@gmail.com>
2025-02-15 02:00:06 +0530

    Update settings.py

--- ToDoApp-Backend/backend/settings.py
+++ ToDoApp-Backend/backend/settings.py
@@ -78,19 +78,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database configuration from .env
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'tsmpQEiSARyAOVhntElbihKyygXxayxM',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '30353',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        },
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
